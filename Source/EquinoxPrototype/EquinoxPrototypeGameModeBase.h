18fbfa42483cc7055b1f1b55415324954b8f6340
Miles <miles.gr.lee@gmail.com>
2023-02-16 16:20:08 -0600

    Initial Commit

---
+++ Source/EquinoxPrototype/EquinoxPrototypeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EquinoxPrototypeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EQUINOXPROTOTYPE_API AEquinoxPrototypeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
